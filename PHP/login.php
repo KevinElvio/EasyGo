ba3451cbe2578630edec9556c5638d55323f79f1
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2023-11-11 15:42:01 +0700

    add folder php, css, asets

---
+++ PHP/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyGo - Solusi Terbaik Penyewaan Transportasi</title>
+    <link rel="icon" href="../Asets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/style-login.css">
+</head>
+<body>
+    <div class="login">
+        <img src="../Asets/img/background-login.png" alt="">
+        <div class="box">
+            <H1>LOGIN</H1>
+            <form>
+                <input type="text">
+                <input type="text">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EasyGo
 Project Pemrograman Web Universitas Jember
+Kelompok 8
+
+Mani'ah Mellow	"222410102051"
+Azriel Amaldany "222410102037"
+Kevin Elvio	    "222410102088"
