131f25953621ee6f9114970e0500900af06210e3
ManiahMellow <jellakus@gmail.com>
2023-11-13 23:15:37 +0700

    fix index

--- PHP/createproduk.php
+++ PHP/createproduk.php
@@ -26,58 +26,15 @@
                 </div>
             </div>
 
-            <div class="form">
-                <label for="input email">Email</label>
-                <input type="email" name="Email" id="" style="display: block;">
-                <label for="input email">Email</label>
-                <input type="email" name="Email" id="" style="display: block;">
-                <label for="input email">Email</label>
-                <input type="email" name="Email" id="" style="display: block;">
-                <!-- <form class="row g-3">
-                    <div class="col-md-6">
-                        <label for="inputEmail4" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4">
+            <form class="row row-cols-lg-auto g-3 align-items-center">
+                <div class="col-12">
+                    <label class="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
+                    <div class="input-group">
+                        <div class="input-group-text"><iconify-icon icon="arcticons:car"></iconify-icon></div>
+                        <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username">
                     </div>
-                    <div class="col-md-6">
-                        <label for="inputPassword4" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4">
-                    </div>
-                    <div class="col-12">
-                        <label for="inputAddress" class="form-label">Address</label>
-                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                    </div>
-                    <div class="col-12">
-                        <label for="inputAddress2" class="form-label">Address 2</label>
-                        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="inputCity" class="form-label">City</label>
-                        <input type="text" class="form-control" id="inputCity">
-                    </div>
-                    <div class="col-md-4">
-                        <label for="inputState" class="form-label">State</label>
-                        <select id="inputState" class="form-select">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                        </select>
-                    </div>
-                    <div class="col-md-2">
-                        <label for="inputZip" class="form-label">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
-                    </div>
-                    <div class="col-12">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck">
-                            <label class="form-check-label" for="gridCheck">
-                                Check me out
-                            </label>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary">Sign in</button>
-                    </div>
-                </form> -->
-            </div>
+                </div>
+            </form>
         </div>
         <!-- <div class="button">
             <button>Kirim</button>
--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,74 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once '../app/init.php';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyGo - Solusi Terbaik Penyewaan Transportasi</title>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <link rel="icon" href="Asets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/landing_page.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+</head>
 
-$app =  new App;
+<body>
+    <div class="atas">
+        <img src="../Asets/img/logo.png" alt="">
+        <div class="tombol">
+            <a href="#">Home</a>
+            <a href="#">History</a>
+            <a href="#">Profil</a>
+        </div>
+        <div class="login-awal">
+            <button>Login</button>
+        </div>
+    </div>
+
+    <div class="isi">
+        <div class="tengah">
+            <p class="satu">Mudahkan Perjalananmu</p>
+            <p class="dua">Sewa Kendaraan <br> Terpercaya di EasyGo</p>
+            <p class="tiga">EasyGo Easy Access Wherever You Go</p>
+            <div class="list">
+                <ul>
+                    <li>Tersedia di Beberapa Kota</li>
+                    <li>Pilih Kendaraanmu Sendiri!</li>
+                    <li>Terpercaya</li>
+                    <li>Tidak Ribet</li>
+                </ul>
+            </div>
+            <div class="daftar-tengah">
+                <button class="btn-daftar">Daftar Sekarang</button>
+            </div>
+            <div class="icon-tengah">
+                <iconify-icon icon="pepicons-pencil:car"></iconify-icon>
+                <iconify-icon icon="carbon:map"></iconify-icon>
+                <iconify-icon icon="healthicons:vespa-motorcycle-outline" style="font-size: 30px;"></iconify-icon>
+                <iconify-icon icon="codicon:workspace-trusted"></iconify-icon>
+            </div>
+        </div>
+
+        <div class="kotak-putih">
+            <div class="isian-kotak">
+                <div class="satu-kiri">
+                    <p class="kiri-pertama">Partner <br> Terpercaya</p>
+                    <p class="isian-kedua">Kepuasan dan kenyamanan <br> anda dijamin disini</p>
+                </div>
+                <div class="dua-tengah">
+                    <p class="tengah-pertama">20+ Rental</p>
+                    <p class="isian-abu">Di Indonesia</p>
+                </div>
+                <div class="tiga-kanan">
+                    <p class="tengah-pertama">120 Pengguna</p>
+                    <p class="isian-abu">Percaya Website Ini</p>
+                </div>
+            </div>
+            <div class="daftar-kotak">
+                <button class="btn-kotak">Coba Gratis!</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
