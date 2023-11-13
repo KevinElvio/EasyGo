e327930f09c439e1229910aea4933c02bf266b39
ManiahMellow <jellakus@gmail.com>
2023-11-13 22:33:21 +0700

    Add Homepage

---
+++ PHP/createproduk.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyGo - Solusi Terbaik Penyewaan Transportasi</title>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <link rel="icon" href="Asets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/createproduk.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    include "navbarrental.php";
+    ?>
+
+    <div class="luar">
+        <div class="pembungkus">
+            <div class="gambar">
+                <h2>Upload Image</h2>
+                <div class="inputan">
+                    <input type="file" name="choose file" id="">
+                </div>
+            </div>
+
+            <div class="form">
+                <label for="input email">Email</label>
+                <input type="email" name="Email" id="" style="display: block;">
+                <label for="input email">Email</label>
+                <input type="email" name="Email" id="" style="display: block;">
+                <label for="input email">Email</label>
+                <input type="email" name="Email" id="" style="display: block;">
+                <!-- <form class="row g-3">
+                    <div class="col-md-6">
+                        <label for="inputEmail4" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="inputEmail4">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputPassword4" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4">
+                    </div>
+                    <div class="col-12">
+                        <label for="inputAddress" class="form-label">Address</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                    </div>
+                    <div class="col-12">
+                        <label for="inputAddress2" class="form-label">Address 2</label>
+                        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputCity" class="form-label">City</label>
+                        <input type="text" class="form-control" id="inputCity">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="inputState" class="form-label">State</label>
+                        <select id="inputState" class="form-select">
+                            <option selected>Choose...</option>
+                            <option>...</option>
+                        </select>
+                    </div>
+                    <div class="col-md-2">
+                        <label for="inputZip" class="form-label">Zip</label>
+                        <input type="text" class="form-control" id="inputZip">
+                    </div>
+                    <div class="col-12">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="gridCheck">
+                            <label class="form-check-label" for="gridCheck">
+                                Check me out
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                    </div>
+                </form> -->
+            </div>
+        </div>
+        <!-- <div class="button">
+            <button>Kirim</button>
+        </div> -->
+    </div>
+</body>
+
+</html>
---
+++ PHP/navbarrental.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyGo - Solusi Terbaik Penyewaan Transportasi</title>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <link rel="icon" href="Asets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/navbarrental.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+</head>
+
+<body>
+    <div class="atas">
+        <img src="../Asets/img/logo.png" alt="">
+        <div class="tombol">
+            <a href="#">Home</a>
+            <a href="#">Produk</a>
+            <a href="#">History</a>
+            <a href="#">Profil</a>
+        </div>
+        <div class="profil">
+            <p>Username</p>
+            <iconify-icon icon="iconamoon:profile-circle-light"></iconify-icon>
+        </div>
+    </div>
+</body>
+
+</html>
