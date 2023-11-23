bee1aeed2d1e93103d4026c74e9b3337b4441c40
AZR88 <azrielamaldany@gmail.com>
2023-11-24 00:29:33 +0700

    update responsive user

--- app/views/user/produk/formpemesanan.php
+++ app/views/user/produk/formpemesanan.php
@@ -13,145 +13,140 @@
     <?php
     // include "navbaruser.php";
     ?>
-
-    <div class="dalamnya">
-        <div class="container1">
-            <div class="judul">
-                EasyGo Rental Form
-            </div>
-            <div class="back">
-                <a href="<?= BASEURL; ?>/produk/detail">
-                    <iconify-icon icon="ph:arrow-up-thin" rotate="-90deg" style="vertical-align: middle;"></iconify-icon>
-                </a>
-            </div>
-            <img src='<?= BASEURL; ?>/img/avanza.jpg' alt="mobil">
+    <div class="container1">
+        <div class="judul">
+            EasyGo Rental Form
         </div>
-        <div class="container2">
-            <div class="box1">
-                <div class="input">
-                    <h3> Nama Penyewa</h3>
-                    <div class="logo">
-                        <iconify-icon icon="iconamoon:profile-circle-thin" width="35" height="35"></iconify-icon></iconify-icon>
-                        <input type="text" placeholder="Nama">
-                    </div>
-                </div>
-                <div class="input">
-                    <h3> Email</h3>
-                    <div class="logo">
-                        <iconify-icon icon="mdi-light:email" width="35" height="35"></iconify-icon>
-                        <input type="text" placeholder="Email">
-                    </div>
-                </div>
-                <div class="input">
-                    <h3> Nama Rental</h3>
-                    <div class="logo">
-                        <iconify-icon icon="arcticons:car" width="35" height="35"></iconify-icon>
-                        <input type="text" placeholder="Nama Rental">
-                    </div>
-                </div>
-                <div class="input">
-                    <h3> Lokasi</h3>
-                    <div class="logo">
-                        <iconify-icon icon="system-uicons:location" width="35" height="35"></iconify-icon>
-                        <input type="text" placeholder="Lokasi">
-                    </div>
-                </div>
-                <div class="input">
-                    <h3>No Telepon</h3>
-                    <div class="logo">
-                        <iconify-icon icon="fluent:call-20-regular" width="35" height="35"></iconify-icon>
-                        <input type="text" placeholder="No Telepon">
-                    </div>
-                </div>
-                <div class="input">
-                    <h3>Harga Sewa</h3>
-                    <div class="logo">
-                        <iconify-icon icon="ph:money-thin" width="35" height="35"></iconify-icon>
-                        <input type="text" placeholder="Harga">
-                    </div>
-                </div>
-
-            </div>
-            <div class="box2">
-                <div class="detail">
-                    <h2>Pilih Tanggal Penyewaan</h2>
-                </div>
-                <div class="rent">
-                    <div class="date">
-                        <H3> Tanggal Peminjaman</H3>
-                        <input type="date" placeholder=" Tanggal peminjaman">
-                    </div>
-                    <div class="date">
-                        <H3> Tanggal Pengembalian</H3>
-                        <input type="date" placeholder=" Tanggal pengembalian">
-                    </div>
-                </div>
-                <table>
-                    <tr>
-                        <th>Minggu</th>
-                        <th>Senin</th>
-                        <th>Selasa</th>
-                        <th>Rabu</th>
-                        <th>Kamis</th>
-                        <th>Jumat</th>
-                        <th>Sabtu</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                    </tr>
-                    <tr>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                        <td>19</td>
-                        <td>20</td>
-                        <td>21</td>
-                    </tr>
-                    <tr>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                        <td>26</td>
-                        <td>27</td>
-                        <td>28</td>
-                    </tr>
-                    <tr>
-                        <td>29</td>
-                        <td>30</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                    </tr>
-                </table>
-
-            </div>
+        <div class="back">
+        <a href="<?= BASEURL; ?>/produk/detail">
+            <iconify-icon icon="ph:arrow-up-thin" rotate="-90deg" style="vertical-align: middle;"></iconify-icon>
+        </a>
         </div>
+        <img src='<?= BASEURL; ?>/img/avanza.jpg' alt="mobil">
+    </div>
+    <div class="container2">
+        <div class="box1">
+            <div class="input">
+                <h3> Nama Penyewa</h3>
+                <div class="logo">
+                    <iconify-icon icon="iconamoon:profile-circle-thin" ></iconify-icon></iconify-icon>
+                    <input type="text" placeholder="Nama">
+                </div>
+            </div>
+            <div class="input">
+                <h3> Email</h3>
+                <div class="logo">
+                    <iconify-icon icon="mdi-light:email" ></iconify-icon>
+                    <input type="text" placeholder="Email">
+                </div>
+            </div>
+            <div class="input">
+                <h3> Nama Rental</h3>
+                <div class="logo">
+                    <iconify-icon icon="arcticons:car" ></iconify-icon>
+                    <input type="text" placeholder="Nama Rental">
+                </div>
+            </div>
+            <div class="input">
+                <h3> Lokasi</h3>
+                <div class="logo">
+                    <iconify-icon icon="system-uicons:location" ></iconify-icon>
+                    <input type="text" placeholder="Lokasi">
+                </div>
+            </div>
+            <div class="input">
+                <h3>No Telepon</h3>
+                <div class="logo">
+                    <iconify-icon icon="fluent:call-20-regular" ></iconify-icon>
+                    <input type="text" placeholder="No Telepon">
+                </div>
+            </div>
+            <div class="input">
+                <h3>Harga Sewa</h3>
+                <div class="logo">
+                    <iconify-icon icon="ph:money-thin" ></iconify-icon>
+                    <input type="text" placeholder="Harga">
+                </div>
+            </div>
 
-        <div class="button-kirim">
-            <button>Rent</button>
+        </div>
+        <div class="box2">
+            <div class="detail">
+                <h2>Pilih Tanggal Penyewaan</h2>
+            </div>
+            <div class="rent">
+                <div class="date">
+                    <H3> Tanggal Peminjaman</H3>
+                    <input type="date" placeholder=" Tanggal peminjaman">
+                </div>
+                <div class="date">
+                    <H3> Tanggal Pengembalian</H3>
+                    <input type="date" placeholder=" Tanggal pengembalian">
+                </div>
+            </div>
+            <table>
+                <tr>
+                    <th>Minggu</th>
+                    <th>Senin</th>
+                    <th>Selasa</th>
+                    <th>Rabu</th>
+                    <th>Kamis</th>
+                    <th>Jumat</th>
+                    <th>Sabtu</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14</td>
+                </tr>
+                <tr>
+                    <td>15</td>
+                    <td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>22</td>
+                    <td>23</td>
+                    <td>24</td>
+                    <td>25</td>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>
+                </tr>
+                <tr>
+                    <td>29</td>
+                    <td>30</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                </tr>
+            </table>
+
         </div>
     </div>
 
-
+    <div class="button-kirim">
+        <button>Rent</button>
+    </div>
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 
 
